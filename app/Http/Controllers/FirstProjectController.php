2632a60255caf5806c0464f070b4b9adb13ee2d4
Ghiftee <gigibetine@gmail.com>
2023-05-30 19:25:50 +0100

    Create laravel app

---
+++ app/Http/Controllers/FirstProjectController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreFirstProjectRequest;
+use App\Http\Requests\UpdateFirstProjectRequest;
+use App\Models\FirstProject;
+
+class FirstProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFirstProjectRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FirstProject $firstProject)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FirstProject $firstProject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFirstProjectRequest $request, FirstProject $firstProject)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FirstProject $firstProject)
+    {
+        //
+    }
+}
